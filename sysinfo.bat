94cb4c7bbbb0dcdc503b24c86b10f8831f7a156e
edirimkus <93425953+edirimkus@users.noreply.github.com>
2024-10-13 23:12:30 +0300

    Update sysinfo.bat

--- sysinfo.bat
+++ sysinfo.bat
@@ -1,4 +1,9 @@
 @echo off
+REM Prompt for computer name
 set /P "CompName=Please enter the computer name: "
+
+REM Fetch and display system information
 systeminfo /s %CompName% | findstr /B /C:"Host Name" /C:"OS Name" /C:"OS Version" /C:"System Boot Time" /C:"System Manufacturer" /C:"System Model" /C:"System Type" /C:"Total Physical Memory"
+
+REM Pause to allow user to view the results
 pause
